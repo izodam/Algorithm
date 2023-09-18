0d6ecc63e8e5907292478ba7581861509657a2d7
madozi <84683989+madozi@users.noreply.github.com>
2023-09-19 00:39:56 +0900

    [Gold IV] Title: 불!, Time: 1216 ms, Memory: 39528 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -4,6 +4,7 @@ input = sys.stdin.readline
 
 r,c = map(int,input().split())
 maze = [list(input()) for _ in range(r)]
+res = 0
 
 #지환이와 불난 곳 저장
 j = deque()
@@ -13,50 +14,51 @@ f = deque()
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-visit_j = [[0]*c for _ in range(r)]
-visit_f = [[0]*c for _ in range(r)]
-
-def fbfs():
-    while f:
-        x,y = f.popleft()
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if not (0<=nx<r and 0<=ny<c):
-                continue
-            if maze[nx][ny]=='#' or visit_f[nx][ny]:
-                continue
-            visit_f[nx][ny] = visit_f[x][y]+1
-            f.append((nx,ny))
-
-
-def jbfs():
-    while j:
-        x,y = j.popleft()
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            #탈출 성공
-            if not (0<=nx<r and 0<=ny<c):
-                print(visit_j[x][y])
-                return
-            if maze[nx][ny]=='#' or visit_j[nx][ny]:
-                continue
-            if visit_f[nx][ny] and visit_j[x][y]+1>=visit_f[nx][ny]:
-                continue
-            visit_j[nx][ny]=visit_j[x][y]+1
-            j.append((nx,ny))
-    print('IMPOSSIBLE')
-    return
-
 for i in range(r):
     for k in range(c):
         if maze[i][k]=='J':
             j.append((i,k))
-            visit_j[i][k] = 1
-        elif maze[i][k]=='F':
+        if maze[i][k]=='F':
             f.append((i,k))
-            visit_f[i][k]=1
 
-fbfs()
-jbfs()
+def bfs():
+    global f,j,res
+    while True:
+        res += 1
+        temp = []
+        while f:
+            x,y = f.popleft()
+            for i in range(4):
+                nx = x+dx[i]
+                ny = y+dy[i]
+                if nx<0 or ny<0 or nx>=r or ny>=c:
+                    continue
+                if maze[nx][ny]=='.' or maze[nx][ny]=='$':
+                    temp.append((nx,ny))
+                    maze[nx][ny] = 'F'
+        f = deque(temp)
+
+        temp=[]
+        while j:
+            x,y = j.popleft()
+            #미로 탈출
+            if x==0 or y==0 or x==r-1 or y==c-1:
+                return res
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if nx < 0 or ny < 0 or nx >= r or ny >= c:
+                    continue
+                if maze[nx][ny] == '.':
+                    temp.append((nx,ny))
+                    #갔던 곳은 $로 표시
+                    maze[x][y] = '$'
+                    maze[nx][ny] = 'J'
+        j = deque(temp)
+        if not j:
+            return False
+
+if bfs():
+    print(res)
+else:
+    print('IMPOSSIBLE')
