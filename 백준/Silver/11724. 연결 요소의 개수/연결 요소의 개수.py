c78562508eb2f1cd173d568e8125f77f09067b04
izodam <84683989+izodam@users.noreply.github.com>
2023-10-23 20:44:58 +0900

    Update 연결 요소의 개수.py

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -20,9 +20,9 @@ def dfs(graph, v, visited):
 
 
 cnt = 0
-for i in range(n+1):
+for i in range(1, n+1):
     if not visited[i]:
         dfs(graph,i,visited)
         cnt += 1
 
-print(cnt-1)
+print(cnt)
