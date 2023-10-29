11a7957fb9ca8bc853343fe14ced955f18d3d7a9
izodam <84683989+izodam@users.noreply.github.com>
2023-10-30 01:43:17 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.py
@@ -0,0 +1,13 @@
+n = int(input())
+pattern = input().split('*')
+length = len(pattern[0]+pattern[1])
+
+for _ in range(n):
+    file = input()
+    if length > len(file):
+        print('NE')
+    else:
+        if pattern[0] == file[:len(pattern[0])] and pattern[1] == file[-len(pattern[1]):]:
+            print("DA")
+        else:
+            print('NE')
