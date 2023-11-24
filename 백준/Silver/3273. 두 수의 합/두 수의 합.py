0839eb9b22e8e3f26a696e2ac7eeb7c0dec81993
izodam <84683989+izodam@users.noreply.github.com>
2023-11-24 23:28:28 +0900

    [Silver III] Title: 두 수의 합, Time: 100 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = list(map(int,input().split()))
+x = int(input())
+arr.sort()
+
+res = 0
+left = 0
+right = n-1
+while left < right:
+    temp = arr[left] + arr[right]
+    if temp == x:
+        res += 1
+        left += 1
+    elif temp < x:
+        left += 1
+    else:
+        right -= 1
+print(res)
