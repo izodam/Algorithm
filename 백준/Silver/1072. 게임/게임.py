6deb8efef1d4f385542c2605c7558b535c48a016
izodam <84683989+izodam@users.noreply.github.com>
2023-11-29 21:19:55 +0900

    [Silver III] Title: 게임, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1072. 게임/게임.py
@@ -0,0 +1,17 @@
+x,y = map(int,input().split())
+z = (y*100) // x
+
+left = 0
+right = x
+res = x
+if z >= 99:
+    print(-1)
+else:
+    while left <= right:
+        mid = (left+right) // 2
+        if ((y+mid)*100) // (x+mid) > z: 
+            res = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    print(res)
