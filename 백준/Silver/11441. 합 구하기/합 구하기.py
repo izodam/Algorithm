ab723a1b1387d211949ec4138d759c3a274c2f72
izodam <84683989+izodam@users.noreply.github.com>
2024-01-05 21:33:01 +0900

    [Silver III] Title: 합 구하기, Time: 4972 ms, Memory: 195300 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.py
@@ -0,0 +1,10 @@
+# 11441번
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = list(map(int,input().split()))
+m = int(input())
+for _ in range(m):
+    i,j = map(int,input().split())
+    print(sum(a[i-1:j]))
