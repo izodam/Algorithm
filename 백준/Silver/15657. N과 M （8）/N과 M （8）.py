8bcea62ef697652debc2f84808ac8378823b390a
izodam <84683989+izodam@users.noreply.github.com>
2024-01-13 08:49:02 +0900

    [Silver III] Title: N과 M (8), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,16 @@
+# 15657번
+n, m =map(int,input().split())
+num = list(map(int,input().split()))
+num.sort()      # 사전 순으로 증가하며 출력되기 위해 받은 숫자들 정렬
+ans = []
+
+def back(x):
+    if len(ans) == m:
+        print(' '.join(map(str,ans)))   # 수열이 m개이면 출력!
+        return
+    for i in range(x,n):
+        ans.append(num[i])
+        back(i)
+        ans.pop()   # 리스트의 가장 마지막 요소 빼고 다음 요소 넣기위해
+
+back(0)
