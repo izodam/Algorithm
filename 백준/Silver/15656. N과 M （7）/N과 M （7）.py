9585e4833c5bf4dbccbd5c1e798a6de130fea000
izodam <84683989+izodam@users.noreply.github.com>
2024-01-12 17:49:13 +0900

    [Silver III] Title: N과 M (7), Time: 1568 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.py
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -1,14 +1,15 @@
-n, m = map(int, input().split())
-nums = sorted(list(map(int, input().split())))
-temp = []
+n, m =map(int,input().split())
+num = list(map(int,input().split()))
+num.sort()      # 사전 순으로 증가하며 출력되기 위해 받은 숫자들 정렬
+ans = []
 
-def dfs():
-    if len(temp) == m:
-        print(*temp)
+def back():
+    if len(ans) == m:
+        print(' '.join(map(str,ans)))   # 수열이 m개이면 출력!
         return
-    for i in range(n):
-            temp.append(nums[i])
-            dfs()
-            temp.pop()
+    for i in num:
+        ans.append(i)
+        back()
+        ans.pop()   # 리스트의 가장 마지막 요소 빼고 다음 요소 넣기위해
 
-dfs()
+back()
