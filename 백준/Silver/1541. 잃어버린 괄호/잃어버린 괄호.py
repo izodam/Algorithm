8f281c2727bb00ba6650725b72d4df37ed706f10
izodam <84683989+izodam@users.noreply.github.com>
2024-01-23 14:32:58 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,8 +1,18 @@
-arr = input().split('-')
-s = 0
-for i in arr[0].split('+'):
-    s += int(i)
-for i in arr[1:]:
+# 1541번
+minus = input().split('-')
+
+# minus의 0번 인덱스는 무조건 res에 더해야함
+hap = 0
+for j in minus[0].split('+'):
+    hap += int(j)
+res = hap
+
+# 그 다음부터는 무조건 빼주기
+for i in minus[1:]:
+    hap = 0
     for j in i.split('+'):
-        s -= int(j)
-print(s)
+        hap += int(j)
+
+    res -= hap
+
+print(res)
