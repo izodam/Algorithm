b2cbb3f18baed2599183892df95cbbbf145b55b1
izodam <84683989+izodam@users.noreply.github.com>
2024-02-06 08:49:52 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 60 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -1,13 +1,14 @@
+# 2346번
 from collections import deque
 n = int(input())
+# 풍선 번호를 얻어내기 위해 enumerate로 생성
 ball = deque(enumerate(map(int,input().split())))
-res = []
 
 while ball:
     idx, paper = ball.popleft()
-    res.append(idx + 1)
+    print(idx+1, end=' ')
+
     if paper > 0:
-        ball.rotate(-(paper-1))
+        ball.rotate(-paper + 1)
     elif paper < 0:
         ball.rotate(-paper)
-print(' '.join(map(str,res)))
