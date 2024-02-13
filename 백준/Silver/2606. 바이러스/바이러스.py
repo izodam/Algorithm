2a48433b04d86041c77779c28d5cbb81e4f998a0
izodam <84683989+izodam@users.noreply.github.com>
2024-02-13 11:01:00 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -3,25 +3,18 @@ def dfs(v):
     stack = [v]
     visited[v] = 1
 
-    # while True:
-    #     for w in graph[v]:
-    #         if not visited[w]:
-    #             visited[w] = 1
-    #             stack.append(w)
-    #             v = w
-    #             break
-    #     else:
-    #         if stack:
-    #             v = stack.pop()
-    #         else:
-    #             break
-
-    while stack:
-        n = stack.pop()
-        for next_node in graph[n]:
-            if not visited[next_node]:
-                visited[next_node] = 1
-                stack.append(next_node)
+    while True:
+        for w in graph[v]:
+            if not visited[w]:
+                visited[w] = 1
+                stack.append(v)
+                v = w
+                break
+        else:
+            if stack:
+                v = stack.pop()
+            else:
+                break
 
 
 n = int(input())
