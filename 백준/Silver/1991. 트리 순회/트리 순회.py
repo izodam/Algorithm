9a051300f190611ca10a324165de49ac125d6faf
izodam <84683989+izodam@users.noreply.github.com>
2024-02-24 14:51:49 +0900

    [Silver I] Title: 트리 순회, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.py
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -1,54 +1,40 @@
 # 전위
 def preorder(t):
-    if t:
-        res_pre.append(tree[t])
-        preorder(tree.index(left[t]))
-        preorder(tree.index(right[t]))
-
+    if t != '.':
+        res_pre.append(t)
+        preorder(tree[t][0])
+        preorder(tree[t][1])
 
 # 중위
 def inorder(t):
-    if t:
-        inorder(tree.index(left[t]))
-        res_in.append(tree[t])
-        inorder(tree.index(right[t]))
+    if t != '.':
+        inorder(tree[t][0])
+        res_in.append(t)
+        inorder(tree[t][1])
 
 # 후위
 def postorder(t):
-    if t:
-        postorder(tree.index(left[t]))
-        postorder(tree.index(right[t]))
-        res_pst.append(tree[t])
-
+    if t != '.':
+        postorder(tree[t][0])
+        postorder(tree[t][1])
+        res_pst.append(t)
 
 
 n = int(input())
-
-tree =  [0] * (n+1)
-left = [0] * (n+1)
-right = [0] * (n+1)
-
-root = 0
+tree = {}
 
 # 트리 만들기
 for i in range(1,n+1):
     node, l, r = input().split()
-    tree[i] = node
-    if l != '.':
-        left[i] = l
-    if r != '.':
-        right[i] = r
-    if node == 'A':
-        root = i
+    tree[node] = [l,r]
 
 res_pre = []
 res_in = []
 res_pst = []
 
-preorder(root)
-inorder(root)
-postorder(root)
-
+preorder('A')
+inorder('A')
+postorder('A')
 
 print(''.join(res_pre))
 print(''.join(res_in))
