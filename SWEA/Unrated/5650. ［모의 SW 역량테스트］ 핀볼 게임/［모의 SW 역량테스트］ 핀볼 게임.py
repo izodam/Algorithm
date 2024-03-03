ce3cd56026b63888e6162fbbc8ac478a2403b45c
izodam <84683989+izodam@users.noreply.github.com>
2024-03-03 23:27:04 +0900

    [Unrated] Title: [모의 SW 역량테스트] 핀볼 게임, Time: 2,691 ms, Memory: 66,900 KB -BaekjoonHub

--- SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.py
+++ SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.py
@@ -1,46 +1,21 @@
 delta = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-direction = [[2, 0, 3, 1],
-             [1, 2, 3, 0],
-             [1, 3, 0, 2],
-             [3, 0, 1, 2],
-             [1, 0, 3, 2]]
-
-def findnew(x, y, block, k):
-    global score
-    # 블록이라면 방향바꾸고 점수 추가
-    if 1 <= block <= 5:
-        k = direction[block - 1][k]
-        score += 1
-    # 웜홀이라면 위치 이동
-    elif block >= 6:
-        for i, j in hole[block]:
-            if i != x and j != y:
-                x, y = i, j
-                break
-
-
-
+direction = [{0: 2, 1: 0, 2: 3, 3: 1},
+             {0: 1, 1: 2, 2: 3, 3: 0},
+             {0: 1, 1: 3, 2: 0, 3: 2},
+             {0: 3, 1: 0, 2: 1, 3: 2},
+             {0: 1, 1: 0, 2: 3, 3: 2}]
 
 # k = delta index
 def move(x, y, k):
     score = 0
     nx, ny = x + delta[k][0], y + delta[k][1]
     while True:
-        # print(nx, ny)
         # 종료 조건
         if nx == x and ny == y:
             return score
         if (nx, ny) in black:
             return score
-
-        # if k == 0:
-        #     # 해당 방향으로 빈 공간이 아닌게 존재하다면
-        #     # 방향 옮겨주고 for문 끝냄
-        #     for i in range(x, n):
-        #         if board[i][y] != 0:
-
-
 
         # 벽 부딪히면 점수 추가
         if nx < 0 or nx >= n or ny < 0 or ny >= n:
@@ -63,12 +38,12 @@ def move(x, y, k):
         nx += delta[k][0]
         ny += delta[k][1]
 
+
 T = int(input())
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     n = int(input())
     # -1 = 블랙홀, 1~5 = 블록, 6~10 = 웜홀
-    board = [list(map(int,input().split())) for _ in range(n)]
-
+    board = [list(map(int, input().split())) for _ in range(n)]
     # 웜홀과 블랙홀 위치 받아놓기
     hole = {}
     black = []
@@ -76,7 +51,7 @@ for tc in range(1, T+1):
         for j in range(n):
             if board[i][j] >= 6:
                 if board[i][j] in hole:
-                    hole[board[i][j]].append((i,j))
+                    hole[board[i][j]].append((i, j))
                 else:
                     hole[board[i][j]] = [(i, j)]
             elif board[i][j] == -1:
@@ -89,7 +64,6 @@ for tc in range(1, T+1):
         for j in range(n):
             if board[i][j] == 0:
                 for k in range(4):
-                    score = move(i,j,k)
-                    # print(score)
+                    score = move(i, j, k)
                     res = max(res, score)
-    print(f'#{tc}',res)
+    print(f'#{tc}', res)
