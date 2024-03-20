c72ef84fe7e3b550cc478a2abfdb4a39f3732085
izodam <84683989+izodam@users.noreply.github.com>
2024-03-20 16:50:36 +0900

    [Silver I] Title: RGB거리, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,14 +1,11 @@
+# 백준 1149
 n = int(input())
-cost = [list(map(int,input().split())) for _ in range(n)]
+house = [list(map(int,input().split())) for _ in range(n)]
 
-for i in range(1,n):
-    # 빨 -> 전 집의 초,파를 선택했을 때의 최소값과 합한 것으로 결정
-    cost[i][0] = min(cost[i-1][1],cost[i-1][2]) + cost[i][0]
+for i in range(1, n):
+    house[i][0] += min(house[i-1][1], house[i-1][2])
+    house[i][1] += min(house[i-1][0], house[i-1][2])
+    house[i][2] += min(house[i-1][0], house[i-1][1])
 
-    # 초
-    cost[i][1] = min(cost[i-1][0],cost[i-1][2]) + cost[i][1]
 
-    # 파
-    cost[i][2] = min(cost[i - 1][0], cost[i - 1][1]) + cost[i][2]
-
-print(min(cost[n-1]))
+print(min(house[-1]))
