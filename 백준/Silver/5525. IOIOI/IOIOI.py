d5e9b2c888f65a73e4c4d83292c1ffa67e884ff9
izodam <84683989+izodam@users.noreply.github.com>
2024-04-01 21:20:29 +0900

    [Silver I] Title: IOIOI, Time: 348 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,20 @@
+n = int(input())
+m = int(input())
+s = input()
+
+cnt = 0
+i = 0
+res = 0
+while i < m-1:
+    if s[i:i+3] == 'IOI':
+        i += 2
+        cnt += 1
+        if cnt == n:
+            res += 1
+            cnt -= 1
+    else:
+        i += 1
+        cnt = 0
+
+
+print(res)
