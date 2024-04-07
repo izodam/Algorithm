9a33896d1d47575f71b7c4519f6a2107b510c22c
izodam <84683989+izodam@users.noreply.github.com>
2024-04-07 19:10:15 +0900

    [Gold IV] Title: 여행 가자, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x < y:
+        par[y] = x
+    else:
+        par[x] = y
+
+def find(x):
+    if x != par[x]:
+        par[x] = find(par[x])
+    return par[x]
+
+n = int(input())
+m = int(input())
+par = [i for i in range(n)]
+for i in range(n):
+    x = list(map(int,input().split()))
+    for j in range(n):
+        if x[j] == 1:
+            union(i, j)
+
+par = [-1] + par
+path = list(map(int,input().split()))
+start = par[path[0]]
+for i in range(1, m):
+    if par[path[i]] != start:
+        print("NO")
+        break
+else:
+    print("YES")
