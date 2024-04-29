ff6455c550188f79f2d120113e2063d26e77bf0d
izodam <84683989+izodam@users.noreply.github.com>
2024-04-29 17:43:34 +0900

    [Gold V] Title: 수 고르기, Time: 5500 ms, Memory: 36148 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -0,0 +1,15 @@
+n, m = map(int,input().split())
+nums = sorted([int(input()) for _ in range(n)])
+
+res = float('inf')
+st, en = 0, 0
+
+while en < n and st < n:
+    if nums[en] - nums[st] >= m:
+        res = min(res, nums[en]-nums[st])
+        st += 1
+    else:
+        en += 1
+
+
+print(res)
