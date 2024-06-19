3b2d99f36f5cd2d8334adfe404902732efb9e067
izodam <84683989+izodam@users.noreply.github.com>
2024-06-19 14:33:23 +0900

    [Gold IV] Title: 연구소, Time: 340 ms, Memory: 114748 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,58 +1,51 @@
-from itertools import combinations
-from collections import deque
+# 14502. 연구소
 import sys
 input = sys.stdin.readline
-
+from itertools import combinations
+from collections import deque
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-def bfs(x, y):
-    q = deque()
-    q.append((x, y))
+def bfs(now):
+    q = deque(virus)
 
     while q:
         x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and tmp[nx][ny] == 0:
-                tmp[nx][ny] = 2
+            if 0 <= nx < n and 0 <= ny < m and now[nx][ny] == 0:
                 q.append((nx, ny))
-
+                now[nx][ny] = 2
 
 
 n, m = map(int,input().split())
-graph = [list(map(int,input().split())) for _ in range(n)]
-
-safe_zone = []
+# 0은 빈 칸, 1은 벽, 2는 바이러스
+board = [list(map(int,input().split())) for _ in range(n)]
+blank = []
 virus = []
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 0:
-            safe_zone.append((i, j))
-        elif graph[i][j] == 2:
-            virus.append((i, j))
 
-tmp = [[] for _ in range(n)]
 res = 0
 
-# 위치 3개 꺼내서 벽 세우기
-for wall in combinations(safe_zone, 3):
-    # tmp = copy.deepcopy(graph)
-    # for i in range(n):
-    #     tmp[i][:] = graph[i][:]
-    tmp = [i.copy() for i in graph]
-    for i, j in wall:
-        tmp[i][j] = 1
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 0:
+            blank.append((i, j))
+        elif board[i][j] == 2:
+            virus.append((i, j))
 
-    # 바이러스 퍼뜨리기
-    for i, j in virus:
-        bfs(i, j)
+# 빈칸 중 3개 골라서 벽 세우기
+for walls in combinations(blank, 3):
+    now = [i.copy() for i in board]
+    for r, c in walls:
+        now[r][c] = 1
+
+    bfs(now)
     cnt = 0
 
-    for r in tmp:
-        cnt += r.count(0)
+    for i in now:
+        cnt += i.count(0)
     res = max(res, cnt)
 
 print(res)
