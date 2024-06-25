7eabd98f13ae9b45f0b7fcff153c143f27e27238
izodam <84683989+izodam@users.noreply.github.com>
2024-06-25 12:41:22 +0900

    [Silver I] Title: 스타트와 링크, Time: 2532 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,21 +1,21 @@
-
+# 14889. 스타트와 링크
+import sys
+input = sys.stdin.readline
 
 def findsum(arr):
-    food1 = arr[:]
-    food2 = [i for i in range(n) if i not in arr]
+    start = arr[:]
+    link = [i for i in range(n) if i not in arr]
 
     res1 = 0
     res2 = 0
 
-    for i in range(len(food1) - 1):
-        for j in range(i+1, len(food1)):
-            res1 += board[food1[i]][food1[j]] + board[food1[j]][food1[i]]
-            res2 += board[food2[i]][food2[j]] + board[food2[j]][food2[i]]
-
+    for i in range(len(start)-1):
+        for j in range(i+1, len(start)):
+            res1 += board[start[i]][start[j]] + board[start[j]][start[i]]
+            res2 += board[link[i]][link[j]] + board[link[j]][link[i]]
     return abs(res1-res2)
 
-
-def dfs(a, x):
+def back(a, x):
     global res
     if x == n // 2:
         now = findsum(arr)
@@ -23,17 +23,12 @@ def dfs(a, x):
         return
     for i in range(a, n):
         arr.append(i)
-        dfs(i+1, x+1)
+        back(i+1, x+1)
         arr.pop()
 
-
-
-T = 1
-for tc in range(1, T+1):
-    n = int(input())
-    # n개의 음식을 두개로 나누기
-    board = [list(map(int,input().split())) for _ in range(n)]
-    arr = []
-    res = 10e10
-    dfs(0,0)
-    print(res)
+n = int(input())
+board = [list(map(int,input().split())) for _ in range(n)]
+arr = []
+res = float('inf')
+back(0, 0)
+print(res)
