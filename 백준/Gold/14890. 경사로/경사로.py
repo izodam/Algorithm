13a0700a1f38303f5742e34ec17252aebb1fe4db
izodam <84683989+izodam@users.noreply.github.com>
2024-06-26 15:42:45 +0900

    [Gold III] Title: 경사로, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.py
+++ 백준/Gold/14890. 경사로/경사로.py
@@ -1,30 +1,57 @@
-#1.
-def chk(line):
-    res = 1
-    for i in range(1, N):
-        # 같은 층이면 1 추가
-        if line[i] == line[i-1]:
-            res += 1
-        # 1 올라감, 기존에 X 만큼 채웠어야함
-        elif line[i] - line[i-1] ==1 and res >= X:
-            res = 1 # 다시 1부터
-        # 1 낮아질때, 0보다 커야지 X 채우고 안 낮아진거임
-        elif line[i-1] - line[i] ==1 and res >= 0:
-            res = 1 - X # 현재 포함해야 되어서 1 하고 X 뺌
+import sys
+input = sys.stdin.readline
+
+# 14890. 경사로
+
+# 낮은 칸과 높은 칸의 높이 차이는 1이여야
+# 경사로는 낮은 칸에 놓이며, L개의 연속된 칸에 모두 접해야 함 -> L개의 연속된 칸이 높이가 모두 같아야 함
+
+def set_load(board):
+    global res
+    for i in range(n):
+        before_height = board[i][0]
+        visited = [0] * n
+
+        idx = 1
+        while idx < n:
+            # 낮아진 경우
+            if before_height - board[i][idx] == 1:
+                if 0 <= idx + l - 1 < n and board[i][idx:idx + l] == [board[i][idx]] * l and 1 not in visited[idx:idx + l]:
+                    before_height = board[i][idx]
+                    visited[idx:idx + l] = [1] * l
+                    idx = idx + l
+                else:
+                    break
+
+            # 높아진 경우
+            elif before_height - board[i][idx] == -1:
+                if 0 <= idx - l < n and board[i][idx - l:idx] == [before_height] * l and 1 not in visited[idx - l:idx]:
+                    before_height = board[i][idx]
+                    idx = idx + 1
+                else:
+                    break
+
+            # 이전과 높이가 같은 경우
+            elif before_height == board[i][idx]:
+                idx += 1
+
+            else:
+                break
+
         else:
-            return 0
-    if res >= 0:
-        return 1
-    return 0
-T = 1
-for tc in range(1,T+1):
-    N, X = map(int, input().split())
-    arr = [list(map(int, input().split())) for _ in range(N)]
-    ans = 0
-    for i in range(N):
-        ans += chk(arr[i])
-        temp = []
-        for j in range(N):
-            temp.append(arr[j][i])
-        ans += chk(temp)
-    print(ans)
+            res += 1
+
+
+n, l = map(int,input().split())
+board = [list(map(int,input().split())) for _ in range(n)]
+
+res = 0
+# 가로 확인
+set_load(board)
+
+# 세로 확인 위해 보드판 전치
+board = list(map(list, zip(*board)))
+
+set_load(board)
+
+print(res)
