b5d8684b513c51c86ca5a80691a879e49353a192
izodam <84683989+izodam@users.noreply.github.com>
2024-06-26 14:39:08 +0900

    [Gold V] Title: 톱니바퀴, Time: 68 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.py
@@ -1,44 +1,62 @@
+import sys
+input = sys.stdin.readline
+
+# 14891. 톱니바퀴
+
+# 서로 맞닿은 톱니의 극이 다르다면, B는 A가 회전한 방향과 반대방향으로 회전
+# 점수
+# 1번 톱니바퀴의 12시방향이 N극이면 0점, S극이면 1점
+# 2번 톱니바퀴의 12시방향이 N극이면 0점, S극이면 2점
+# 3번 톱니바퀴의 12시방향이 N극이면 0점, S극이면 4점
+# 4번 톱니바퀴의 12시방향이 N극이면 0점, S극이면 8점
 from collections import deque
 
-def cheak_mag(s, n,rotate):
-    if 0 <= n+1 < 4 and n+1 not in arr:
-        if mag[n][2] != mag[n+1][6]:
-            cheak.append((n+1, rotate*(-1)))
-            arr.append(n+1)
-            cheak_mag(s, n+1,rotate*(-1))
+# 회전해야하는 톱니바퀴 찾기
+def find_numbers(n, direction):
+    # 오른쪽 톱니바퀴 체크 -> n의 index 2와 n+1의 index 6이 맞닿음
+    if 0 <= n + 1 < 4 and n + 1 not in rotate_number:
+        if wheel[n][2] != wheel[n+1][6]:
+            check.append((n+1, direction*(-1)))
+            rotate_number.append(n+1)
+            find_numbers(n+1, direction*(-1))
 
-    if 0 <= n-1 < 4 and n-1 not in arr:
-        if mag[n-1][2] != mag[n][6]:
-            cheak.append((n-1, rotate*(-1)))
-            arr.append(n-1)
-            cheak_mag(s, n-1,rotate*(-1))
+    # 왼쪽 톱니바퀴 체크 -> n의 index 6와 n-1의 index 2이 맞닿음
+    if 0 <= n-1 < 4 and n - 1 not in rotate_number:
+        if wheel[n][6] != wheel[n-1][2]:
+            check.append((n-1, direction*(-1)))
+            rotate_number.append(n-1)
+            find_numbers(n-1, direction*(-1))
 
-    return
+def turn():
+    for n, direction in check:
+        wheel[n].rotate(direction)
 
-mag = [deque(map(int,input().strip())) for _ in range(4)]
+
+
+# N극은 0, S극은 1
+# 12시방향부터 시계방향 순서대로
+wheel = [deque(list(input().strip())) for _ in range(4)]
+
+# 회전 횟수
 k = int(input())
-# N 극이 0 으로, S 극이 1
-# 0의 idx 2와 1의 idx 6 붙어있음
-# 1의 idx 2와 2의 idx 6 붙어있음
-# 2의 idx 2와 3의 idx 6 붙어있음
-res = 0
 
-# k번 회전
+score = 0
+
+# 회전 방법
+# 톱니바퀴 번호, 방향 (1 : 시계, -1 : 반시계)
 for _ in range(k):
-    # 시계방향이 1, 반시계 방향이 -1
-    # 시계방향이 리스트 rotate 1
-    # 반시계가 리스트 rotate -1
-    n, direction = map(int,input().split())
-    # 돌렸을 때 체크해야하는 자석과 방향들 넣어주기
-    cheak = [(n-1, direction)]
-    # 중복 제거 위해서 리스트 하나 더 만듦...
-    arr = [n-1]
-    cheak_mag(n-1, n-1, direction)
-    # 회전 시작
-    for n,direction in cheak:
-        mag[n].rotate(direction)
+    n, direction = map(int, input().split())
+    n -= 1
+
+    check = [(n, direction)]
+    # 회전시킬 톱니바퀴의 번호
+    rotate_number = [n]
+
+    find_numbers(n, direction)
+    turn()
 
 for i in range(4):
-    if mag[i][0]:
-        res += 2**i
-print(res)
+    score += int(wheel[i][0]) * (2 ** i)
+
+print(score)
+
