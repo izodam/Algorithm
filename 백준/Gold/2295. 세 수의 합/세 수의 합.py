a2ce34180f2d2077dc126df6d0ddad56a0ff5ecc
izodam <84683989+izodam@users.noreply.github.com>
2024-07-20 16:55:37 +0900

    [Gold IV] Title: 세 수의 합, Time: 512 ms, Memory: 65940 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.py
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.py
@@ -1,26 +1,17 @@
-import sys
-input = sys.stdin.readline
-def find_res():
-    while True:
-        check_number = u.pop()
-
-        for i in range(len(u)):
-            left = 0
-            right = len(u) - 1
-
-            while left <= right:
-                now = u[i] + u[left] + u[right]
-                if now == check_number:
-                    print(check_number)
-                    return
-                elif now < check_number:
-                    left += 1 
-                else:
-                    right -= 1
-
-
 n = int(input())
-u = [int(input()) for _ in range(n)]
-u.sort()
+u = set()
+for _ in range(n):
+    u.add(int(input()))
+sums = set()
 
-find_res()
+ans = []
+for i in u:
+    for j in u:
+        sums.add(i+j)
+
+
+for i in u:
+    for j in u:
+        if (i-j) in sums:
+            ans.append(i)
+print(max(ans))
