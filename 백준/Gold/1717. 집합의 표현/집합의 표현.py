c85a2917d47b64762a50d5b52cd7e54840d90eb5
izodam <84683989+izodam@users.noreply.github.com>
2024-08-25 20:29:09 +0900

    [Gold V] Title: 집합의 표현, Time: 284 ms, Memory: 71112 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,32 +1,33 @@
 import sys
 input = sys.stdin.readline
-n,m = map(int,input().split())
-
+n, m = map(int,input().split())
 graph = [i for i in range(n+1)]
 
-def union(a, b):
-    a = find(a)
-    b = find(b)
-    if a < b:
-        graph[a] = b
-    else:
-        graph[b] = a
-
+# 포함 확인
 def find(x):
+    # 최상단 root 노드 찾기
     if graph[x] != x:
         graph[x] = find(graph[x])
     return graph[x]
 
 
+# 합치기
+def union(a, b):
+    a = find(a)
+    b = find(b)
 
+    # 더 높은 인덱스를 가진 쪽이 작은 쪽을 흡수
+    if a < b:
+        graph[a] = b
+    else:
+        graph[b] = a
 
 for i in range(m):
-    x, a, b = list(map(int,input().split()))
-    # x가 0이면
-    if not x:
-        union(a, b)
-    else:
+    x, a, b = map(int,input().split())
+    if x == 1:
         if find(a) == find(b):
             print('YES')
         else:
             print('NO')
+    else:
+        union(a, b)
