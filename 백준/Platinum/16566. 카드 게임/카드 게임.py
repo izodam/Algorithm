9ec82a18334024d981659a881f56e859b2d43a34
izodam <84683989+izodam@users.noreply.github.com>
2024-08-25 20:05:53 +0900

    [Platinum V] Title: 카드 게임, Time: 1712 ms, Memory: 530044 KB -BaekjoonHub

--- 백준/Platinum/16566. 카드 게임/카드 게임.py
+++ 백준/Platinum/16566. 카드 게임/카드 게임.py
@@ -1,33 +1,28 @@
 import sys
 input = sys.stdin.readline
 
-def binary_search(cards, card):
-    start = 0
-    end = len(cards)
+from bisect import bisect_right
 
-    while start <= end:
-        mid = (start + end) // 2
-        if cards[mid] == card:
-            return mid+1
-        elif cards[mid] > card:
-            end = mid - 1
-        else: 
-            start = mid + 1
-    return start
+def find(x):
+    if x != parents[x]:
+        parents[x] = find(parents[x])
+    return parents[x]
 
+def union(x, y):
+    if y >= m:
+        return
+    x = find(x)
+    y = find(y)
+    parents[x] = y
 
 n, m, k = map(int, input().split())
 cards = sorted(list(map(int, input().split())))
 su = list(map(int, input().split()))
 
-visited = [0] * m
+parents = [i for i in range(m)]
 
 for su_card in su:
-    idx = binary_search(cards, su_card)
-    # print(idx)
-    for i in range(idx, m):
-        if not visited[i]:
-            visited[i] = 1
-            print(cards[i])
-            break
-    # print(cards)
+    idx = bisect_right(cards, su_card)
+    idx = find(idx)
+    print(cards[idx])
+    union(idx, idx+1)
