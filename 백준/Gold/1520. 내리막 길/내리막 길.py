8502acdcdf9fc814ac9ffdddf2f9d10ba832dc2f
izodam <84683989+izodam@users.noreply.github.com>
2024-09-09 08:49:48 +0900

    [Gold III] Title: 내리막 길, Time: 108 ms, Memory: 44136 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,29 +1,28 @@
 import sys
+sys.setrecursionlimit(100000)
 input = sys.stdin.readline
-from collections import deque
-
-m,n = map(int,input().split())
-map = [list(map(int,input().split())) for _ in range(m)]
-
-dp = [[-1]*n for _ in range(m)]
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-cnt = 0
-
-def dfs(x,y):
-    if x == m-1 and y == n-1:
+def dfs(x, y):
+    if x == n-1 and y == m-1:
         return 1
+
     if dp[x][y] == -1:
         dp[x][y] = 0
         for i in range(4):
-            nx = x+ dx[i]
+            nx = x + dx[i]
             ny = y + dy[i]
-            if 0<=nx<m and 0<=ny<n and map[nx][ny]<map[x][y]:
-                dp[x][y] += dfs(nx,ny)
+
+            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] < board[x][y]:
+                dp[x][y] += dfs(nx, ny)
     return dp[x][y]
 
-print(dfs(0,0))
 
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
 
+dp = [[-1] * m for _ in range(n)]
+
+print(dfs(0, 0))
