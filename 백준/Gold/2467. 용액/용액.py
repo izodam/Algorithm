3d1172d0712e7a41ff65fa519d49b7f9e2ebf63f
izodam <84683989+izodam@users.noreply.github.com>
2024-09-12 17:37:36 +0900

    [Gold V] Title: 용액, Time: 144 ms, Memory: 42036 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+liquid = list(map(int, input().split()))
+
+min_value = float('inf')
+res = []
+
+start = 0
+end = n-1
+
+while start < end:
+    if liquid[start] + liquid[end] == 0:
+        print(liquid[start], liquid[end])
+        break
+    elif abs(liquid[start] + liquid[end]) < min_value:
+        min_value = abs(liquid[start] + liquid[end])
+        res = [liquid[start], liquid[end]]
+    
+    if liquid[start] + liquid[end] < 0:
+        start += 1
+    else:
+        end -= 1
+else: 
+
+    print(*res)
