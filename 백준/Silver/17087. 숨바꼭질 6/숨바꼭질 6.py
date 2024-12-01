b741a2d1b8cb753ebde01ca534ee0a39f518970c
izodam <84683989+izodam@users.noreply.github.com>
2024-12-01 15:48:39 +0900

    [Silver II] Title: 숨바꼭질 6, Time: 104 ms, Memory: 44032 KB -BaekjoonHub

---
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+# 최대공약수
+def gcd(a, b):
+    while b:
+        mod = b
+        b = a % b
+        a = mod
+    return a
+
+n, s = map(int,input().split())
+a = list(map(int,input().split()))
+dif = list(set(abs(a[i] - s) for i in range(n)))
+d = min(dif)
+for i in range(len(dif)):
+    d = gcd(dif[i], d)
+print(d)
